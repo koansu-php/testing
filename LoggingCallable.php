63869c93f35f6f51bed9089bccf7cc9bfbf53f80
Michael Tils <mtils@web-utils.de>
2022-10-01 09:39:17 +0200

    Implemented next bunch of classes

---
+++ LoggingCallable.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Koansu\Testing;
+
+use Countable;
+
+/**
+ * The LoggingCallable allows to fast testing listener calls
+ * It logs all calls and their args.
+ **/
+class LoggingCallable implements Countable
+{
+    /**
+     * Here all calls are logged.
+     *
+     * @var array
+     **/
+    protected $calls = [];
+
+    /**
+     * @var callable
+     **/
+    protected $handler;
+
+    /**
+     * Optionally pass a callable to process the actual call.
+     *
+     * @param callable|null $handler (optional)
+     **/
+    public function __construct(callable $handler = null)
+    {
+        $this->handler = $handler;
+    }
+
+    /**
+     * The fake callable.
+     **/
+    public function __invoke()
+    {
+        $this->calls[] = func_get_args();
+        if ($this->handler) {
+            return call_user_func($this->handler, ...func_get_args());
+        }
+        return null;
+    }
+
+    /**
+     * Returns the argument with idx from a call of this object.
+     * If no callIndex is given, the last one is used.
+     *
+     * @param int $idx       The idx of the arg
+     * @param int $callIndex (optional) The index of the call (0 is the first call)
+     *
+     * @return mixed
+     **/
+    public function arg(int $idx, int $callIndex = -1)
+    {
+        $args = $this->args($callIndex);
+
+        return $args[$idx];
+    }
+
+    /**
+     * Returns the arguments from a call of this object.
+     * If no callIndex is given, the last one is used.
+     *
+     * @param int $callIndex (optional) The index of the call (0 is the first call)
+     *
+     * @return mixed
+     **/
+    public function args(int $callIndex = -1)
+    {
+        $callIndex = $callIndex == -1 ? count($this->calls) - 1 : $callIndex;
+
+        return $this->calls[$callIndex];
+    }
+
+    /**
+     * Returns how many times it was invoked.
+     *
+     * @return int
+     **/
+    public function count() : int
+    {
+        return count($this->calls);
+    }
+}
